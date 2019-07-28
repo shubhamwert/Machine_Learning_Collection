162800c8e5b23d7ee302e9e62d59949f51ebfc1c
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-07-29 01:32:49 +0530

    pandas practice

--- small_projects/pandas_revision/basic_pandas.py
+++ small_projects/pandas_revision/basic_pandas.py
@@ -4,6 +4,53 @@ df=pd.read_csv('sample_data.csv')
 
 df.shape
 plt.plot(df)
-df.describe()
+df.head()
 
-!pip install tensorflow==2.0.0-alpha0 
+
+#AutoMobileData
+
+AutoMobileDataFrame=pd.read_csv('Automobile_data.csv')
+
+
+AutoMobileDataFrame.head(10)
+AutoMobileDataFrame.describe()
+
+#working with missing data
+AutoMobileDataFrame.count()
+AutoMobileDataFrame.isnull().columns.value_counts()
+AutoMobileDataFrame.isnull().sum()
+AutoMobileDataFrame.fillna('0',mehtod='pad')
+df = pd.read_csv('Automobile_data.csv', na_values={
+'price':["?","n.a"],
+'stroke':["?","n.a"],
+'horsepower':["?","n.a"],
+'peak-rpm':["?","n.a"],
+'average-mileage':["?","n.a"]})
+print (df.isnull().sum())
+
+
+df[['company','price']][df.price==df['price'].max()]
+
+
+df[df.company=='toyota']
+
+
+# alternative
+d1=df.groupby('company')
+d1.get_group('toyota')
+
+
+
+
+
+df['company']. value_counts()
+
+m=df.groupby('company')
+m['company','price'].max()
+
+
+# AutoMobileDataFrame.replace(to_replace='?',value='nan',inplace=True)
+# AutoMobileDataFrame.isna().sum(axis=0)
+# for column in AutoMobileDataFrame.isnull().columns.values.tolist():
+#     print(column)
+#     print( AutoMobileDataFrame.isnull()[column].value_counts())
